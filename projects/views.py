7b6af9fb88aff52f69f3168adeb04a1d80532a4d
Erika <o.erika.o@gmail.com>
2021-03-01 14:46:38 -0500

    create url linking - app_name path name and args

--- projects/views.py
+++ projects/views.py
@@ -5,3 +5,7 @@ from projects.models import Project
 def all_projects(request):
   projects = Project.objects.all()
   return render(request, '/projects/all_projects.html', {'projects': projects})
+
+def project_detail(request, pk): #will receive pk from url
+  project = Project.objects.get(pk=pk)
+  return render(request, 'projects/detail.html', {'project': project})
