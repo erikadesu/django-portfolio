79a578e336359c2b0ddfe3b855fa8ef3faeb5a9f
Erika <o.erika.o@gmail.com>
2021-03-01 16:15:21 -0500

    fix routes and edit templates

--- projects/views.py
+++ projects/views.py
@@ -4,7 +4,7 @@ from projects.models import Project
 # Create your views here.
 def all_projects(request):
   projects = Project.objects.all()
-  return render(request, '/projects/all_projects.html', {'projects': projects})
+  return render(request, 'projects/all_projects.html', {'projects': projects})
 
 def project_detail(request, pk): #will receive pk from url
   project = Project.objects.get(pk=pk)
